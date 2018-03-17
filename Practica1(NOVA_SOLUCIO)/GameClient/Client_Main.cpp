d8bc01ae59096067f8cdbb7f6efaf032b0e01fc7
PauBlanes_ENTI <paublanespardo@enti.cat>
2018-03-17 20:33:33 +0100

    nova solucio que si que va

---
+++ Practica1(NOVA_SOLUCIO)/GameClient/Client_Main.cpp
@@ -0,0 +1,14 @@
+
+#include "GameEngine.h"
+#include "Card.h"
+
+GameEngine engine;
+
+int main()
+{
+	engine.start();
+	
+	
+	system("pause");
+	return 0;
+}
---
+++ Practica1(NOVA_SOLUCIO)/GameLib/Card.cpp
@@ -0,0 +1,10 @@
+#include "Card.h"
+
+/*Card::Card(int num, CardColor c, Vector2f pos) {
+	number = num;
+	color = c;
+	position = pos;
+	//std::string cardNum = number + "-" + color;
+	//std::cout << cardNum << std::endl;
+	//texture.loadFromFile("img/" + cardNum);
+}*/
